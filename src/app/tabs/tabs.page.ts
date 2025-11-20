74f8f6a414fd49a908c1f62a451073673efee1e4
Marcos Borges <110690829+marcosborg@users.noreply.github.com>
2025-11-20 16:16:42 +0000

    refactor: update tab icon and label for 'Condução'; remove browserslist file

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { home, receipt, documents, chatbox, person } from 'ionicons/icons';
+import { home, receipt, documents, stopwatch, person } from 'ionicons/icons';
 
 @Component({
     selector: 'app-tabs',
@@ -13,6 +13,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ home, receipt, documents, chatbox, person });
+    addIcons({ home, receipt, documents, stopwatch, person });
   }
 }
